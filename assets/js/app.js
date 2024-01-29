8282bdd855d6a857cb56fd73d0f2c592641e4db8
Matias380 <matias380@hotmail.com>
2024-01-29 14:42:28 -0300

    Funcionalidades
    Cambios a las funcionalidades de javascript

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,40 +1,28 @@
-// Nav hamburgerburger selections
-const burger = document.querySelector("#burger-menu");
-const ul = document.querySelector("nav ul");
-const nav = document.querySelector("nav");
+document.addEventListener("DOMContentLoaded", () => {
+  // Seleccionar elementos del DOM
+  const burger = document.querySelector("#burger-menu");
+  const ul = document.querySelector("nav ul");
+  const scrollUp = document.querySelector("#scroll-up");
+  const navLink = document.querySelectorAll(".nav-link");
 
-// Scroll to top selection
-const scrollUp = document.querySelector("#scroll-up");
+  // Hamburger menu function
+  burger.addEventListener("click", () => {
+    ul.classList.toggle("show");
+  });
 
-// Select nav links
-const navLink = document.querySelectorAll(".nav-link");
+  // Close hamburger menu when a link is clicked
+  navLink.forEach((link) => {
+    link.addEventListener("click", () => {
+      ul.classList.remove("show");
+    });
+  });
 
-// Hamburger menu function
-burger.addEventListener("click", () => {
-  ul.classList.toggle("show");
-});
-
-// Close hamburger menu when a link is clicked
-navLink.forEach((link) =>
-  link.addEventListener("click", () => {
-    ul.classList.remove("show");
-  })
-);
-
-// scroll to top functionality
-scrollUp.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: "smooth",
+  // Scroll to top functionality
+  scrollUp.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
   });
 });
-
-@keyframes slideText {
-  0% {
-    transform: translateX(100%);
-  }
-  100% {
-    transform: translateX(-100%);
-  }
-}
